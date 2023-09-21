6a02d21ecbca08f45962654ce31a95541079911e
Ilias Brinias <elliotpatra@gmail.com>
2023-09-21 20:14:45 +0300

    first push

---
+++ src/main/java/com/unipi/msc/smartalertapi/SmartAlertApiApplication.java
@@ -0,0 +1,13 @@
+package com.unipi.msc.smartalertapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAlertApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAlertApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unipi/msc/smartalertapi/SmartAlertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipi.msc.smartalertapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAlertApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
